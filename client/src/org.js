1969ae2236d8266994237539ed58a4aa747851e2
yaozy <yaozy@126.com>
2019-01-16 23:48:40 +0800

    update

--- client/src/org.js
+++ client/src/org.js
@@ -116,6 +116,14 @@ flyingon.widget({
 
         grid.columns(4).items(['北京', '天津', '河北', '山西', '内蒙古', '辽宁', '吉林', '黑龙江', '上海', '江苏', '浙江', '安徽', '福建', '江西', '山东', '河南', '湖北', '湖南', '广东', '广西', '海南', '重庆', '四川', '贵州', '云南', '西藏', '陕西', '甘肃', '青海', '宁夏', '新疆', '香港', '澳门', '台湾']);
 
+        grid.on('change', function (event) {
+
+            if (event.target.column.name() === 'provincename')
+            {
+                dataset.uniqueId(event.target.row.id).set('cityname', cities[event.value][0]);
+            }
+        });
+
         grid.columns(5).onopening = function () {
 
             this.items(cities[this.row.data.provincename]);
